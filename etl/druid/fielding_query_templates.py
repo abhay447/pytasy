ed53544dd339f614b1b8fd46e97c4c3aac55a0ee
abhay447 <abhay447@gmail.com>
2023-02-11 00:45:04 +0530

    Added match count in fielding stats

--- etl/druid/fielding_query_templates.py
+++ etl/druid/fielding_query_templates.py
@@ -4,5 +4,5 @@ fielding_stats_template = Template("""
 with base_data as (
   select * from delivery_records WHERE __time > '$start_date' AND __time < '$end_date'  AND "match_type" = 'T20' $additional_filters
 )
-SELECT sum(wicket_sum) as fielding_dismissals from base_data where "wicket_fielder_id"= '$fielder_id'
+SELECT sum(wicket_sum) as fielding_dismissals, count(distinct match_id) as match_count from base_data where "wicket_fielder_id"= '$fielder_id'
 """)
