021b5ed8aac0dd69a2e1b5434f8c7b0f5be43239
abhay447 <abhay447@gmail.com>
2023-02-12 16:39:51 +0530

    added window lag features in fielder stats

--- etl/spark/pre_aggregate/agg_t20_fielder_stats_by_match.py
+++ etl/spark/pre_aggregate/agg_t20_fielder_stats_by_match.py
@@ -1,19 +1,36 @@
-from pyspark.sql.functions import sum, col
+from pyspark.sql import functions as f
+from pyspark.sql.window import Window
 from common_requirements import t20_df_with_boundaries
 
 output_path = '/home/abhay/work/dream11/processed_output/t20_fielder_match_stats'
 
-fielder_relevant_dimensions = ["dt","match_id","wicket_fielder_id", "wicket_fielder_name", "bowler_name", "bowler_id"] 
+fielder_relevant_dimensions = ["dt","match_id","wicket_fielder_id", "wicket_fielder_name"] 
 fielder_relevant_metrics = ["is_dismissed"]
 
-t20_batter_fielder_df = t20_df_with_boundaries\
+t20_fielder_df = t20_df_with_boundaries\
     .select(fielder_relevant_dimensions+fielder_relevant_metrics) \
-    .filter(col("wicket_fielder_id").isNotNull())
+    .filter(f.col("wicket_fielder_id").isNotNull())
 
-t20_batter_fielder_df_per_month = t20_batter_fielder_df.groupBy(fielder_relevant_dimensions) \
+t20_fielder_df_per_month = t20_fielder_df.groupBy(fielder_relevant_dimensions) \
     .agg(
-        sum("is_dismissed").alias("wicket_sum")
+        f.sum("is_dismissed").alias("fielding_wicket_sum")
     )
 
-t20_batter_fielder_df_per_month.write.format("parquet").partitionBy(["dt","match_id"]).mode("overwrite").save(output_path)
+w_300d = (
+    Window.partitionBy("wicket_fielder_id","wicket_fielder_name")
+    .orderBy(f.unix_timestamp(f.col("dt").cast("timestamp")))
+    .rangeBetween(-300*86400, Window.currentRow)
+)
+w_1000d = (
+    Window.partitionBy("wicket_fielder_id","wicket_fielder_name")
+    .orderBy(f.unix_timestamp(f.col("dt").cast("timestamp")))
+    .rangeBetween(-1000*86400, Window.currentRow)
+)
 
+windowed_stats_df = t20_fielder_df_per_month.select(
+    "dt","match_id","wicket_fielder_id","wicket_fielder_name","fielding_wicket_sum",
+    f.sum("fielding_wicket_sum").over(w_300d).alias("fielding_wicket_sum_300D"),
+    f.sum("fielding_wicket_sum").over(w_1000d).alias("fielding_wicket_sum_1000D")
+)
+
+windowed_stats_df.write.format("parquet").partitionBy(["dt","match_id"]).mode("overwrite").save(output_path)
