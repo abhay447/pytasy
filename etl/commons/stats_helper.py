e51655a5cce19fb1a1e8fabe42b3f7b8e5457838
abhay447 <abhay447@gmail.com>
2023-02-13 01:55:42 +0530

    hadling nulls in fantasy udf

--- etl/commons/stats_helper.py
+++ etl/commons/stats_helper.py
@@ -1,12 +1,24 @@
+from typing import Optional
 from pyspark.sql.functions import udf
 from pyspark.sql.types import LongType
 
-def __extract_batting_fantasy_points(batter_runs: int, dismissals: int, balls_faced: int, boundaries_count: int, sixes_count: int ):
+def __extract_batting_fantasy_points(
+        batter_runs: Optional[int], dismissals: Optional[int],
+        balls_faced: Optional[int], boundaries_count: Optional[int], sixes_count: Optional[int] ):
     fantasy_points = 0
-    is_out: bool = dismissals > 0
-    is_duck = batter_runs==0 and is_out
+    is_out: bool = dismissals is not None and dismissals > 0
+    is_duck = batter_runs is not None and batter_runs==0 and is_out
+    if batter_runs is None:
+        batter_runs = 0
+    if balls_faced is None:
+        balls_faced = 0
+    
     strike_rate = batter_runs*100.0/balls_faced if balls_faced > 0 else 100
-    fantasy_points = batter_runs * 1 + boundaries_count * 1 + sixes_count * 2
+    fantasy_points = batter_runs * 1 
+    if boundaries_count is not None:
+        fantasy_points += boundaries_count * 1
+    if sixes_count is not None:
+        fantasy_points += sixes_count * 2
     # handle half century
     if batter_runs >=50 and batter_runs < 100:
         fantasy_points += 8
@@ -25,8 +37,16 @@ def __extract_batting_fantasy_points(batter_runs: int, dismissals: int, balls_fa
         fantasy_points -= 2
     return fantasy_points
 
-def __extract_bowling_fantasy_points(total_runs: int, wickets: int, deliveries: int, maidens: int):
+def __extract_bowling_fantasy_points(total_runs: Optional[int], wickets: Optional[int], deliveries: Optional[int], maidens: Optional[int]):
     fantasy_points = 0
+    if total_runs is None:
+        total_runs = 0
+    if deliveries is None:
+        deliveries = 0
+    if wickets is None:
+        wickets = 0
+    if maidens is None:
+        maidens = 0
     economy_rate = total_runs*6.0/deliveries if deliveries > 0 else 6
     fantasy_points = wickets * 25 + maidens * 8
     # handle 4 wickets
@@ -42,15 +62,15 @@ def __extract_bowling_fantasy_points(total_runs: int, wickets: int, deliveries: 
         fantasy_points += 4
     return fantasy_points
 
-def __extract_fielding_fantasy_points(fielding_wickets: int):
-    fantasy_points = 0
-    fantasy_points = fielding_wickets * 7 # avergae to 7 to account for various dismisaals mechanisms
+def __extract_fielding_fantasy_points(fielding_wickets: Optional[int]):
+    fantasy_points = fielding_wickets * 7 if fielding_wickets is not None else 0 # avergae to 7 to account for various dismisaals mechanisms
     return fantasy_points
 
 def get_fantasy_points(
-        batter_runs: int, dismissals: int, balls_faced: int, boundaries_count: int, sixes_count: int,
-        total_runs: int, wickets: int, deliveries: int, maidens: int,
-        fielding_wickets: int
+        batter_runs: Optional[int], dismissals: Optional[int], balls_faced: Optional[int], boundaries_count: Optional[int], 
+        sixes_count: Optional[int],
+        total_runs: Optional[int], wickets: Optional[int], deliveries: Optional[int], maidens: Optional[int],
+        fielding_wickets: Optional[int]
     ):
     return __extract_batting_fantasy_points(batter_runs, dismissals, balls_faced, boundaries_count, sixes_count) \
         + __extract_bowling_fantasy_points(total_runs, wickets, deliveries, maidens) \
